8a2bcbd165109c6e1df0d348649406f0e0e7d4ec
LisaPaliashchuk <yelizaveta.paliashchuk@Orchestr8.com>
2024-02-29 10:05:31 +0100

    added teskScore.js (HW2.1.3)

---
+++ taskScore.js
@@ -0,0 +1,12 @@
+let studentName = 'Yelizaveta Paliashchuk'
+
+let introduction = 100
+let git = 319
+let js = 40
+
+let total = introduction + git + js
+
+let averageScore = total / 3
+console.log(
+  `${studentName}'s average score for the first three modules is ${averageScore} points.`
+)
