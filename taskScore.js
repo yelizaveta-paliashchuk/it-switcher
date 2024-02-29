4e448b5700cd67df4f5119e40ca5fef503603c68
LisaPaliashchuk <yelizaveta.paliashchuk@Orchestr8.com>
2024-02-29 10:35:50 +0100

    modified task.js (HW_2.1.4)

--- taskScore.js
+++ taskScore.js
@@ -1,4 +1,6 @@
 let studentName = 'Yelizaveta Paliashchuk'
+let nameParts = studentName.split(' ')
+let formattedName = nameParts[0].slice(0, 1) + '. ' + nameParts[1]
 
 let introduction = 100
 let git = 319
@@ -8,5 +10,5 @@ let total = introduction + git + js
 
 let averageScore = total / 3
 console.log(
-  `${studentName}'s average score for the first three modules is ${averageScore} points.`
+  `${formattedName}'s average score for the first three modules is ${averageScore} points.`
 )
